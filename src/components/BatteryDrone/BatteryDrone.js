87f16594d5eae6a767d0d81919c9c1a32a190bf8
rishabnagota <rishabnagota@gmail.com>
2024-07-05 08:43:38 +0530

    update

--- src/components/BatteryDrone/BatteryDrone.js
+++ src/components/BatteryDrone/BatteryDrone.js
@@ -4,6 +4,9 @@ import Battery1 from '../../assets/BatteryDetImages/DroneImageBig.png'
 import Battery2 from '../../assets/BatteryDetImages/DroneImagesmall1.png'
 import Battery3 from '../../assets/BatteryDetImages/DroneImagesmall2.png'
 import Battery4 from '../../assets/BatteryDetImages/DroneImagesmall3.png'
+import plus from '../../assets/Signs/plus.png'
+import minus from '../../assets/Signs/minus.png'
+import delivery from '../../assets/Signs/Delivery.png'
 
 const DroneBattery = () => {
   const [quantity, setQuantity] = useState(2);
@@ -38,41 +41,46 @@ const DroneBattery = () => {
         </div>
         </div>
       </div>
-      <div className={styles.purchaseInfo}>
-        <div className={styles.price}>
-          <span>Price</span>
-          <div className={styles.right}>
+      <div className={styles.purchaseinfo}>
+      <div className={styles.price}>
+        <span>Price</span>
+        <div className={styles.right}>
           <span className={styles.originalPrice}>₹ 49,000</span>
           <span className={styles.discountedPrice}>₹ 38,232</span>
           <span className={styles.perPiece}>/Piece (GST inc.)</span>
-          </div>
         </div>
-        <div className={styles.delivery}>
-          <span>Delivery</span>
-          <span>Free</span>
-        </div>
-        <div className={styles.pincode}>
-          <label>Delivery location pincode</label>
-          <input type="text" placeholder="Pincode" />
-          <button>Check Pincode</button>
-        </div>
-        <div className={styles.quantity}>
-          <label>Select quantity</label>
-          <div className={styles.quantitySelector}>
-            <button onClick={decreaseQuantity}>-</button>
-            <span>{quantity}</span>
-            <button onClick={increaseQuantity}>+</button>
-          </div>
-        </div>
-        <div className={styles.extraInfo}>
-          <p>Maximum Order Quantity is 5 set. Need a <a href="#">Bulk Order</a> or <a href="#">Custom order?</a></p>
-          <p>Need a custom order or have any doubts? <a href="#">Call us</a> or <a href="#">Whatsapp</a></p>
-        </div>
-        <div className={styles.shipping}>
-          <span>🚚 Ships within 48 hrs after receiving order</span>
-        </div>
-        <button className={styles.buyButton}>Buy Now</button>
       </div>
+      <div className={styles.delivery}>
+        <span>Delivery</span>
+        <span>Free</span>
+      </div>
+      <div className={styles.pincode}>
+        <label>Delivery location pincode</label>
+        <div className={styles.check}>
+        <input type="text" placeholder="Pincode" />
+        <button>Check Pincode</button>
+        </div>
+      </div>
+      <div className={styles.quantity}>
+        <label>Select quantity</label>
+        <div className={styles.quantitySelector}>
+        <div className={styles.minus}>
+          <button onClick={decreaseQuantity}><img src={minus}/></button></div>
+          <span>{quantity}</span>
+          <div className={styles.plus}>
+          <button onClick={increaseQuantity}><img src={plus}/></button></div>
+        </div>
+      </div>
+      <div className={styles.extraInfo}>
+        <p>Maximum Order Quantity is 6 set. Need a <a href="#">Bulk Order</a> or <a href="#">Custom order?</a></p>
+        <p>Need a custom order or have any doubts? <a href="#">Call us</a> or <a href="#">Whatsapp</a></p>
+      </div>
+      <div className={styles.shipping}>
+        <img src={delivery}/>
+        <span>Ships within 48 hrs after receiving order</span>
+      </div>
+      <button className={styles.buyButton}>Buy Now</button>
+    </div>
     </div>
   );
 };
